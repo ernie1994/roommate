c4931c37ea1b08cb3a3530d14538ca38047769dc
garcie67 <ernie1994@gmail.com>
2019-08-03 11:02:29 -0700

    Fixed NavBar bug

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -8,23 +8,21 @@ class SearchResult extends React.Component {
 
     render() {
         return (
-            <div>
-                <Card className="mb-3">
-                    <Row className="no-gutters">
-                        <Col md="4">
-                            <CardImg src={this.props.image} alt=""></CardImg>
-                        </Col>
-                        <Col md="8">
-                            <CardBody>
-                                <CardTitle tag="h1">{this.props.name}</CardTitle>
-                                <CardSubtitle tag="h6">{this.props.address}</CardSubtitle>
-                                <br></br>
-                                <CardText>{this.props.description}</CardText>
-                            </CardBody>
-                        </Col>
-                    </Row>
-                </Card>
-            </div>
+            <Card className="mb-3">
+                <Row className="no-gutters">
+                    <Col md="4">
+                        <CardImg src={this.props.image} alt=""></CardImg>
+                    </Col>
+                    <Col md="8">
+                        <CardBody>
+                            <CardTitle tag="h1">{this.props.name}</CardTitle>
+                            <CardSubtitle tag="h6">{this.props.address}</CardSubtitle>
+                            <br></br>
+                            <CardText>{this.props.description}</CardText>
+                        </CardBody>
+                    </Col>
+                </Row>
+            </Card>
         );
     }
 }
