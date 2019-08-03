a1c2f816fbd65ee59910fe4ec298db9706ad761b
Yaroslav Petsenyuk <yaroslav.petsenyuk@gmail.com>
2019-08-03 11:13:15 -0700

    commiting my code in order to pull. Code might be broken so will have to look out for this during next merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Nav from "./components/Nav/index"
 import SearchResults from "./components/SearchResults.js";
 
 class App extends React.Component {
-    render() {
-        return (
-             <Nav />
-            <SearchResults />
-        );
-    }
+  render() {
+    return (
+      <div className="container">
+        <Nav />
+        <SearchResults />
+      </div>
+    );
+  }
 }
 
 export default App;
