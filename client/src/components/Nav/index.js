efefd7c50edfe0ada72b77101b77f7e0ecb8f614
Yaroslav Petsenyuk <yaroslav.petsenyuk@gmail.com>
2019-08-03 10:47:23 -0700

    Scipted a basic layout of the Navbar. Included the nav in the home page.

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="/">
+                Home
+      </a>
+            <a className="navbar-brand" href="#">
+                Post a Room
+      </a>
+            <a className="navbar-brand" href="#">
+                Search Rooms
+      </a>
+        </nav>
+    );
+}
+
+export default Nav;
